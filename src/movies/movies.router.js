903f27a263c823946a9b8320955844cd17c73438
Clarisse <clarisse421@berkeley.edu>
2023-10-16 14:50:58 -0400

    created and seeded all 5 tables needed for the app

---
+++ src/movies/movies.router.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const controller = require("./movies.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+module.exports = router;
---
+++ src/reviews/reviews.router.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const controller = require("./reviews.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+module.exports = router;
