a1d750203efb4090bc4acb9b2e4cf2a853d4b40d
Clarisse <clarisse421@berkeley.edu>
2023-10-18 16:28:52 -0400

    Final submission

--- src/db/migrations/20231016121706_createReviewsTable.js
+++ src/db/migrations/20231016121706_createReviewsTable.js
@@ -1,20 +1,15 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reviews", (table) => {
-    table.increments("review_id").primary();
+    table.increments("review_id");
     table.text("content");
     table.integer("score");
+    table.timestamps(true, true);
+
     table.integer("critic_id").unsigned().notNullable();
-    table
-      .foreign("critic_id")
-      .references("critic_id")
-      .inTable("critics")
-      .onDelete("CASCADE");
+    table.foreign("critic_id").references("critic_id").inTable("critics");
+
     table.integer("movie_id").unsigned().notNullable();
-    table
-      .foreign("movie_id")
-      .references("movie_id")
-      .inTable("movies")
-      .onDelete("CASCADE");
+    table.foreign("movie_id").references("movie_id").inTable("movies");
   });
 };
 
